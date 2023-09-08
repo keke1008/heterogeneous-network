2e98fd50e94470fb9f4348023d69d741c137dfed
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-09-08 21:17:41 +0900

    feat: Stream型の再設計

--- lib/nb/src/nb/stream/types.h
+++ lib/nb/src/nb/stream/types.h
@@ -1,78 +1,192 @@
 #pragma once
 
-#include <etl/functional.h>
-#include <etl/utility.h>
+#include <etl/span.h>
 #include <nb/poll.h>
-#include <util/tuple.h>
 
 namespace nb::stream {
-    class FiniteStreamWriter;
-
-    class FiniteStreamReader;
-
-    class StreamReader {
-      public:
-        using Item = uint8_t;
-
-        virtual nb::Poll<Item> read() = 0;
-
-      private:
-        nb::Poll<void> fill_single(FiniteStreamWriter &writer);
-
+    /**
+     * バイト列を読み取ることが出来るストリーム．
+     */
+    class ReadableStream {
       public:
         /**
-         * `this`のデータを全て`ws`に吐き出す．
-         * @param ws `this`からデータを吐き出される，書き込み可能な有限ストリーム
+         * 呼び出されたときに確実に読み取れるバイト数を返す．
+         * @return 読み取れるバイト数
          */
-        template <typename... FiniteWriters>
-        nb::Poll<void> fill_all(FiniteWriters &&...ws) {
-            (fill_single(etl::forward<FiniteWriters>(ws)).is_ready() && ...);
-            return nb::ready();
-        }
-    };
+        virtual uint8_t readable_count() const = 0;
 
-    class FiniteStreamReader : public StreamReader {
-      public:
-        virtual nb::Poll<void> wait_until_empty() = 0;
-    };
+        /**
+         * 1バイト読み取る．
+         * @pre `readable_count() > 0`
+         * @return 読み取ったバイト
+         */
+        virtual uint8_t read() = 0;
 
-    class StreamWriter {
-        friend class StreamReader;
-
-      public:
-        using Item = uint8_t;
-
-      protected:
-        virtual nb::Poll<void> wait_until_writable() = 0;
-        virtual void write(Item) = 0;
-
-      private:
-        inline nb::Poll<void> drain_single(FiniteStreamReader &reader) {
-            while (wait_until_writable().is_ready()) {
-                write(POLL_UNWRAP_OR_RETURN(reader.read()));
-
-                if (reader.wait_until_empty().is_ready()) {
-                    return nb::ready();
-                }
+        /**
+         * `buffer`に読み取ったバイト列を書き込む．
+         * @pre `readable_count() >= buffer.size()`
+         * @param buffer 書き込み先
+         */
+        virtual void read(etl::span<uint8_t> buffer) {
+            for (auto &byte : buffer) {
+                byte = read();
             }
-            return nb::pending;
-        }
-
-        inline nb::Poll<void> drain_single(FiniteStreamReader &&reader) {
-            return drain_single(reader);
-        }
-
-      public:
-        /**
-         * `rs`のデータ全てを`this`に吸い込む．
-         * @param rs `this`にデータを吸い込まれる，読み込み可能な有限ストリーム
-         */
-        template <typename... FiniteReader>
-        nb::Poll<void> drain_all(FiniteReader &&...rs) {
-            bool is_ready = (drain_single(etl::forward<FiniteReader>(rs)).is_ready() && ...);
-            return is_ready ? nb::ready() : nb::pending;
         }
     };
 
-    class FiniteStreamWriter : public StreamWriter {};
+    /**
+     * バイト列を書き込むことが出来るストリーム．
+     *
+     * `write`関数はまだ書き込めるかどうかを返すが，これは書き込まれた内容によって，
+     * 動的に書き込めるバイト数が変わる場合に対応するためである．
+     */
+    class WritableStream {
+      public:
+        /**
+         * 呼び出されたときに確実に書き込めるバイト数を返す．
+         * @return 書き込めるバイト数
+         */
+        virtual uint8_t writable_count() const = 0;
+
+        /**
+         * 1バイト書き込む．
+         * @pre `writable_count() > 0`
+         * @param byte 書き込むバイト
+         * @return まだ書き込める場合は`true`，そうでない場合は`false`
+         */
+        virtual bool write(uint8_t) = 0;
+
+        /**
+         * `buffer`のバイト列を書き込む．
+         * @pre `writable_count() >= buffer.size()`
+         * @param buffer 書き込むバイト列
+         * @return まだ書き込める場合は`true`，そうでない場合は`false`
+         */
+        virtual bool write(etl::span<uint8_t> buffer) {
+            for (uint8_t byte : buffer) {
+                write(byte);
+            }
+            return writable_count() > 0;
+        }
+    };
+
+    /**
+     * バイト列を`WritableStream`に吐き出すことが出来る型．
+     *
+     * `write_to`が右辺値参照を受け取るオーバーロードがないが，
+     * これは繰り返し`write_to(HogeWritableStream{})`のように間違った呼ばれ方をすると，
+     * 正しく書き込んだデータを蓄積できないためである．
+     */
+    class ReadableBuffer : public ReadableStream {
+      public:
+        /**
+         * `this`のデータを吐き出せるだけ全て`destination`に吐き出す．
+         * @param destination `this`からデータを吐き出される，書き込み可能なストリーム
+         * @return 書き込みが完了した場合は`nb::ready()`，そうでない場合は`nb::pending`
+         */
+        virtual nb::Poll<void> write_to(WritableStream &destination) {
+            bool continue_ = true;
+            while (continue_ && readable_count() > 0) {
+                continue_ = destination.write(read());
+            }
+            return readable_count() > 0 ? nb::pending : nb::ready();
+        }
+    };
+
+    /**
+     * バイト列の読み込み元をetl::span<uint8_t>で指定できる`ReadableBuffer`．
+     *
+     * `write_to`のオーバーライドを簡単にするために用意した．
+     */
+    class SpannableReadableBuffer : public ReadableBuffer {
+      public:
+        /**
+         * `length`バイト分の`etl::span<uint8_t>`を返す．
+         * 戻り値のspanのデータはすべて消費される．
+         * @pre `readable_count() >= length`
+         * @param length 取得するバイト数
+         * @return `length`バイト分の`etl::span<uint8_t>`
+         */
+        virtual etl::span<uint8_t> take_span(uint8_t length) = 0;
+
+        nb::Poll<void> write_to(WritableStream &destination) override {
+            bool continue_ = true;
+            while (continue_ && readable_count() > 0) {
+                uint8_t count = etl::min(readable_count(), destination.writable_count());
+                continue_ = destination.write(take_span(count));
+            }
+            return readable_count() > 0 ? nb::pending : nb::ready();
+        }
+    };
+
+    /**
+     * バイト列を`ReadableStream`に吸い込ませることが出来る型．
+     *
+     * `read_from`が右辺値参照を受け取るオーバーロードがない理由は`ReadableBuffer`を参照．
+     */
+    class WritableBuffer : public WritableStream {
+      public:
+        /**
+         * `source`からデータを吸い込めるだけ全て`this`に吸い込む．
+         * @param source `this`にデータを吸い込まれる，読み込み可能なストリーム
+         * @return 読み込みが完了した場合は`nb::ready()`，そうでない場合は`nb::pending`
+         */
+        virtual nb::Poll<void> read_from(ReadableStream &source) {
+            uint8_t read_count = etl::min(writable_count(), source.readable_count());
+            for (uint8_t i = 0; i < read_count; i++) {
+                write(source.read());
+            }
+            return writable_count() > 0 ? nb::pending : nb::ready();
+        }
+    };
+
+    /**
+     * バイト列の書き込み先をetl::span<uint8_t>で指定できる`WritableBuffer`．
+     *
+     * `read_from`のオーバーライドを簡単にするために用意した．
+     */
+    class SpannableWritableBuffer : public WritableBuffer {
+      public:
+        /**
+         * `length`バイト分の`etl::span<uint8_t>`を返す．
+         * 戻り値のspanのデータはすべて消費される．
+         * @pre `writable_count() >= length`
+         * @param length 取得するバイト数
+         * @return `length`バイト分の`etl::span<uint8_t>`
+         */
+        virtual etl::span<uint8_t> take_span(uint8_t length) = 0;
+
+        nb::Poll<void> read_from(ReadableStream &source) override {
+            uint8_t count = etl::min(writable_count(), source.readable_count());
+            auto span = take_span(count);
+            source.read(span);
+            return writable_count() > 0 ? nb::pending : nb::ready();
+        }
+    };
+
+    /**
+     * `reader`から読み取ったバイト列を`wb`に書き込む．
+     *
+     * 全ての`wb`を埋めることが出来た場合は`nb::ready()`を返し，そうでない場合は`nb::pending`を返す．
+     *
+     * @note `wb`は`WritableBuffer`を継承する必要がある．
+     */
+    template <typename... WBuffers>
+    nb::Poll<void> write_all(ReadableStream &reader, WBuffers &&...wb) {
+        const bool is_ready = (wb.read_from(reader).is_ready() && ...);
+        return is_ready ? nb::ready() : nb::pending;
+    }
+
+    /**
+     * `rb`から読み取ったバイト列を`writer`に書き込む．
+     *
+     * 全ての`rb`を空にできた場合は`nb::ready()`を返し，そうでない場合は`nb::pending`を返す．
+     *
+     * @note `rb`は`ReadableBuffer`を継承する必要がある．
+     */
+    template <typename... RBuffers>
+    nb::Poll<void> read_all(WritableStream &writer, RBuffers &&...rb) {
+        const bool is_ready = (rb.write_to(writer).is_ready() && ...);
+        return is_ready ? nb::ready() : nb::pending;
+    }
 } // namespace nb::stream
--- test/test_nb/stream/types.cpp
+++ test/test_nb/stream/types.cpp
@@ -1,106 +1,254 @@
 #include <doctest.h>
 
-#include <nb/stream.h>
-#include <vector>
+#include <etl/array.h>
+#include <nb/stream/types.h>
 
 using namespace nb::stream;
 
-struct TestStreamReader final : FiniteStreamReader {
-    std::vector<uint8_t> data_;
+template <size_t N>
+struct TestReadableBuffer final : public ReadableBuffer {
+    etl::array<uint8_t, N> buffer_;
+    uint8_t index_{0};
+
+    TestReadableBuffer(const TestReadableBuffer &) = delete;
+    TestReadableBuffer(TestReadableBuffer &&) = delete;
+    TestReadableBuffer &operator=(const TestReadableBuffer &) = delete;
+    TestReadableBuffer &operator=(TestReadableBuffer &&) = delete;
 
     template <typename... Ts>
-    TestStreamReader(Ts... ts) : data_{static_cast<uint8_t>(ts)...} {}
+    TestReadableBuffer(Ts... ts) : buffer_{static_cast<uint8_t>(ts)...} {}
 
-    nb::Poll<Item> read() override {
-        if (data_.empty()) {
-            return nb::pending;
+    explicit TestReadableBuffer(etl::array<uint8_t, N> buffer) : buffer_(std::move(buffer)) {}
+
+    uint8_t readable_count() const override {
+        return buffer_.size() - index_;
+    }
+
+    uint8_t read() override {
+        return buffer_[index_++];
+    }
+};
+
+template <size_t N>
+struct TestWritableBuffer final : public WritableBuffer {
+    etl::array<uint8_t, N> buffer_;
+    uint8_t index_{0};
+
+    TestWritableBuffer() {
+        buffer_.fill(0);
+    }
+
+    uint8_t writable_count() const override {
+        return buffer_.size() - index_;
+    }
+
+    bool write(uint8_t byte) override {
+        if (writable_count() > 0) {
+            buffer_[index_++] = byte;
         }
-        auto data = data_.front();
-        data_.erase(data_.begin());
-        return data;
-    }
-
-    nb::Poll<void> wait_until_empty() override {
-        return data_.empty() ? nb::ready() : nb::pending;
+        return writable_count() > 0;
     }
 };
 
-struct TestStreamWriter final : StreamWriter {
-    std::vector<uint8_t> data_;
+template <size_t N>
+struct TestSpannableReadableBuffer : public SpannableReadableBuffer {
+    etl::array<uint8_t, N> buffer_;
+    uint8_t index_{0};
 
-    nb::Poll<void> wait_until_writable() override {
-        return nb::ready();
+    TestSpannableReadableBuffer(std::initializer_list<uint8_t> bytes) : buffer_{bytes} {}
+
+    uint8_t readable_count() const override {
+        return buffer_.size() - index_;
     }
 
-    void write(Item item) override {
-        data_.push_back(item);
+    uint8_t read() override {
+        return buffer_[index_++];
+    }
+
+    etl::span<uint8_t> take_span(uint8_t length) override {
+        return etl::span{buffer_.data() + index_, length};
     }
 };
 
-struct TestFixedStreamWriter final : FiniteStreamWriter {
-    std::vector<uint8_t> data_;
+template <size_t N>
+struct TestSpannableWritableBuffer : public SpannableWritableBuffer {
+    etl::array<uint8_t, N> buffer_;
+    uint8_t index_{0};
 
-    nb::Poll<void> wait_until_writable() override {
-        if (data_.size() >= 3) {
-            return nb::pending;
+    TestSpannableWritableBuffer() {
+        buffer_.fill(0);
+    }
+
+    uint8_t writable_count() const override {
+        return buffer_.size() - index_;
+    }
+
+    bool write(uint8_t byte) override {
+        if (writable_count() > 0) {
+            buffer_[index_++] = byte;
         }
-        return nb::ready();
+        return writable_count() > 0;
     }
 
-    void write(Item item) override {
-        data_.push_back(item);
+    etl::span<uint8_t> take_span(uint8_t length) override {
+        return etl::span{buffer_.data() + index_, length};
     }
 };
 
-TEST_CASE("Reader") {
-    SUBCASE("empty") {
-        TestStreamReader reader{};
-        CHECK_EQ(reader.read(), nb::pending);
+TEST_CASE_TEMPLATE("write_to default implementation", T, TestWritableBuffer<3>, TestSpannableWritableBuffer<3>) {
+    SUBCASE("same capacity") {
+        T buffer;
+        TestReadableBuffer<3> readable{42, 43, 44};
+        readable.write_to(buffer);
+        CHECK_EQ(buffer.buffer_, etl::array<uint8_t, 3>{42, 43, 44});
+        CHECK_EQ(readable.readable_count(), 0);
     }
 
-    SUBCASE("single") {
-        TestStreamReader reader{42};
-        CHECK_EQ(reader.read(), 42);
-        CHECK_EQ(reader.read(), nb::pending);
+    SUBCASE("extra capacity") {
+        T buffer;
+        TestReadableBuffer<2> readable{42, 43};
+        readable.write_to(buffer);
+        CHECK_EQ(buffer.buffer_, etl::array<uint8_t, 3>{42, 43});
+        CHECK_EQ(readable.readable_count(), 0);
     }
 
-    SUBCASE("multiple") {
-        TestStreamReader reader{42, 43, 44};
-        CHECK_EQ(reader.read(), 42);
-        CHECK_EQ(reader.read(), 43);
-        CHECK_EQ(reader.read(), 44);
-        CHECK_EQ(reader.read(), nb::pending);
-    }
-
-    SUBCASE("fill writer") {
-        TestStreamReader reader{42, 43, 44, 45, 46, 47};
-        TestFixedStreamWriter writer1{};
-        TestFixedStreamWriter writer2{};
-        reader.fill_all(writer1, writer2);
-        CHECK_EQ(writer1.data_, std::vector<uint8_t>{42, 43, 44});
-        CHECK_EQ(writer2.data_, std::vector<uint8_t>{45, 46, 47});
+    SUBCASE("less capacity") {
+        T buffer;
+        TestReadableBuffer<4> readable{42, 43, 44, 45};
+        readable.write_to(buffer);
+        CHECK_EQ(buffer.buffer_, etl::array<uint8_t, 3>{42, 43, 44});
+        CHECK_EQ(readable.readable_count(), 1);
     }
 }
 
-TEST_CASE("Writer") {
-    TestStreamWriter writer{};
-
-    SUBCASE("empt reader") {
-        TestStreamReader reader{};
-        writer.drain_all(reader);
-        CHECK_EQ(writer.data_, std::vector<uint8_t>{});
+TEST_CASE("WritableBuffer::read_from default implementation") {
+    SUBCASE("same capacity") {
+        TestWritableBuffer<3> buffer;
+        TestReadableBuffer<3> readable{42, 43, 44};
+        buffer.read_from(readable);
+        CHECK_EQ(buffer.buffer_, etl::array<uint8_t, 3>{42, 43, 44});
+        CHECK_EQ(readable.readable_count(), 0);
     }
 
-    SUBCASE("single reader") {
-        TestStreamReader reader{42};
-        writer.drain_all(reader);
-        CHECK_EQ(writer.data_, std::vector<uint8_t>{42});
+    SUBCASE("extra capacity") {
+        TestWritableBuffer<6> buffer;
+        TestReadableBuffer<3> readable{42, 43, 44};
+        buffer.read_from(readable);
+        CHECK_EQ(buffer.buffer_, etl::array<uint8_t, 6>{42, 43, 44});
+        CHECK_EQ(readable.readable_count(), 0);
     }
 
-    SUBCASE("multiple readers") {
-        TestStreamReader reader1{1, 2, 3};
-        TestStreamReader reader2{11, 12, 13};
-        writer.drain_all(reader1, reader2);
-        CHECK_EQ(writer.data_, std::vector<uint8_t>{1, 2, 3, 11, 12, 13});
+    SUBCASE("less capacity") {
+        TestWritableBuffer<2> buffer;
+        TestReadableBuffer<3> readable{42, 43, 44};
+        buffer.read_from(readable);
+        CHECK_EQ(buffer.buffer_, etl::array<uint8_t, 2>{42, 43});
+        CHECK_EQ(readable.readable_count(), 1);
+    }
+}
+
+TEST_CASE_TEMPLATE("read_from default implementation", T, TestWritableBuffer<3>, TestSpannableWritableBuffer<3>) {
+    SUBCASE("same capacity") {
+        T buffer;
+        TestReadableBuffer<3> readable{42, 43, 44};
+        buffer.read_from(readable);
+        CHECK_EQ(buffer.buffer_, etl::array<uint8_t, 3>{42, 43, 44});
+        CHECK_EQ(readable.readable_count(), 0);
+    }
+
+    SUBCASE("extra capacity") {
+        T buffer;
+        TestReadableBuffer<2> readable{42, 43};
+        buffer.read_from(readable);
+        CHECK_EQ(buffer.buffer_, etl::array<uint8_t, 3>{42, 43});
+        CHECK_EQ(readable.readable_count(), 0);
+    }
+
+    SUBCASE("less capacity") {
+        T buffer;
+        TestReadableBuffer<4> readable{42, 43, 44, 45};
+        buffer.read_from(readable);
+        CHECK_EQ(buffer.buffer_, etl::array<uint8_t, 3>{42, 43, 44});
+        CHECK_EQ(readable.readable_count(), 1);
+    }
+}
+
+TEST_CASE("write_all") {
+    TestWritableBuffer<2> buffer1;
+    TestWritableBuffer<2> buffer2;
+    TestWritableBuffer<2> buffer3;
+
+    SUBCASE("single WritableBuffer") {
+        TestReadableBuffer<2> readable{42, 43};
+        write_all(readable, buffer1);
+        CHECK_EQ(buffer1.buffer_, etl::array<uint8_t, 2>{42, 43});
+        CHECK_EQ(readable.readable_count(), 0);
+    }
+
+    SUBCASE("multiple WritableBuffer with same capacity") {
+        TestReadableBuffer<6> readable{42, 43, 44, 45, 46, 47};
+        write_all(readable, buffer1, buffer2, buffer3);
+        CHECK_EQ(buffer1.buffer_, etl::array<uint8_t, 2>{42, 43});
+        CHECK_EQ(buffer2.buffer_, etl::array<uint8_t, 2>{44, 45});
+        CHECK_EQ(buffer3.buffer_, etl::array<uint8_t, 2>{46, 47});
+        CHECK_EQ(readable.readable_count(), 0);
+    }
+
+    SUBCASE("multiple WritableBuffer with extra capacity") {
+        TestReadableBuffer<3> readable{42, 43, 44};
+        write_all(readable, buffer1, buffer2, buffer3);
+        CHECK_EQ(buffer1.buffer_, etl::array<uint8_t, 2>{42, 43});
+        CHECK_EQ(buffer2.buffer_, etl::array<uint8_t, 2>{44, 0});
+        CHECK_EQ(buffer3.buffer_, etl::array<uint8_t, 2>{0, 0});
+        CHECK_EQ(readable.readable_count(), 0);
+    }
+
+    SUBCASE("multiple WritableBuffer with less capacity") {
+        TestReadableBuffer<7> readable{42, 43, 44, 45, 46, 47, 48};
+        write_all(readable, buffer1, buffer2, buffer3);
+        CHECK_EQ(buffer1.buffer_, etl::array<uint8_t, 2>{42, 43});
+        CHECK_EQ(buffer2.buffer_, etl::array<uint8_t, 2>{44, 45});
+        CHECK_EQ(buffer3.buffer_, etl::array<uint8_t, 2>{46, 47});
+        CHECK_EQ(readable.readable_count(), 1);
+    }
+}
+
+TEST_CASE("read_all") {
+    TestReadableBuffer<2> buffer1{42, 43};
+    TestReadableBuffer<2> buffer2{44, 45};
+    TestReadableBuffer<2> buffer3{46, 47};
+
+    SUBCASE("single ReadableBuffer") {
+        TestWritableBuffer<2> write;
+        read_all(write, buffer1);
+        CHECK_EQ(write.buffer_, etl::array<uint8_t, 2>{42, 43});
+        CHECK_EQ(buffer1.readable_count(), 0);
+    }
+
+    SUBCASE("multiple WritableBuffer with same capacity") {
+        TestWritableBuffer<6> write;
+        read_all(write, buffer1, buffer2, buffer3);
+        CHECK_EQ(write.buffer_, etl::array<uint8_t, 6>{42, 43, 44, 45, 46, 47});
+        CHECK_EQ(buffer1.readable_count(), 0);
+        CHECK_EQ(buffer2.readable_count(), 0);
+        CHECK_EQ(buffer3.readable_count(), 0);
+    }
+
+    SUBCASE("multiple WritableBuffer with extra capacity") {
+        TestWritableBuffer<3> write;
+        read_all(write, buffer1, buffer2, buffer3);
+        CHECK_EQ(write.buffer_, etl::array<uint8_t, 3>{42, 43, 44});
+        CHECK_EQ(buffer1.readable_count(), 0);
+        CHECK_EQ(buffer2.readable_count(), 1);
+        CHECK_EQ(buffer3.readable_count(), 2);
+    }
+
+    SUBCASE("multiple WritableBuffer with less capacity") {
+        TestWritableBuffer<7> write;
+        read_all(write, buffer1, buffer2, buffer3);
+        CHECK_EQ(write.buffer_, etl::array<uint8_t, 7>{42, 43, 44, 45, 46, 47, 0});
+        CHECK_EQ(buffer1.readable_count(), 0);
+        CHECK_EQ(buffer2.readable_count(), 0);
+        CHECK_EQ(buffer3.readable_count(), 0);
     }
 }
