40a13978c926cd672771ced98706f68b61c02bed
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-09-23 15:26:10 +0900

    feat: SingleLineWritableBufferの変更

--- lib/nb/src/nb/stream/single_line.h
+++ lib/nb/src/nb/stream/single_line.h
@@ -1,21 +1,90 @@
 #pragma once
 
+#include <debug_assert.h>
 #include <etl/array.h>
 #include <etl/span.h>
+#include <etl/vector.h>
 #include <nb/stream/types.h>
 
 namespace nb::stream {
+    namespace {
+        inline bool is_complete_line(etl::span<const uint8_t> span) {
+            if (span.size() < 2) {
+                return false;
+            }
+            auto last = span.last<2>();
+            return last[0] == '\r' && last[1] == '\n';
+        }
+    } // namespace
+
+    /**
+     * `MAX_LENGTH`以下の長さの1行を書き込むバッファ．
+     * `MAX_LENGTH`を超える長さの行を書き込んではいけない．
+     */
     template <uint8_t MAX_LENGTH>
     class SingleLineWritableBuffer final : public nb::stream::WritableBuffer {
-        etl::array<uint8_t, MAX_LENGTH> buffer_;
-        uint8_t index_ = 0;
+        static_assert(MAX_LENGTH >= 2, "buffer must be able to contain CRLF");
+
+        etl::vector<uint8_t, MAX_LENGTH> buffer_;
 
       private:
         inline bool is_complete() const {
-            if (index_ < 2) {
-                return false;
+            return is_complete_line(buffer_);
+        }
+
+      public:
+        nb::Poll<void> write_all_from(nb::stream::ReadableStream &source) override {
+            if (is_complete()) {
+                return nb::ready();
             }
-            return buffer_[index_ - 2] == '\r' && buffer_[index_ - 1] == '\n';
+
+            uint8_t readable_count = source.readable_count();
+            uint8_t writable_count = buffer_.available();
+            uint8_t count = etl::min(readable_count, writable_count);
+            for (int i = 0; i < count; i++) {
+                buffer_.push_back(source.read());
+                if (is_complete()) {
+                    return nb::ready();
+                }
+            }
+
+            DEBUG_ASSERT(!buffer_.full(), "line is too long");
+            return nb::pending;
+        }
+
+        nb::Poll<etl::span<const uint8_t>> poll() const {
+            if (is_complete()) {
+                return nb::ready(etl::span(buffer_.begin(), buffer_.end()));
+            }
+            return nb::pending;
+        }
+    };
+
+    /**
+     * 最大`MAX_LENGTH`の長さの1行を書き込むバッファ．
+     * `MAX_LENGTH`を超える長さの行は無視される．
+     */
+    template <uint8_t MAX_LENGTH>
+    class MaxLengthSingleLineWrtableBuffer final : public nb::stream::WritableBuffer {
+        static_assert(MAX_LENGTH >= 2, "buffer must be able to contain CRLF");
+
+        etl::vector<uint8_t, MAX_LENGTH> buffer_;
+
+        bool is_complete() const {
+            return is_complete_line(buffer_);
+        }
+
+        nb::Poll<void> discard_line(nb::stream::ReadableStream &source) {
+            bool is_cr = buffer_.size() == 0 ? false : buffer_.back() == '\r';
+            uint8_t readable_count = source.readable_count();
+            for (uint8_t i = 0; i < readable_count; i++) {
+                uint8_t byte = source.read();
+                if (is_cr && byte == '\n') {
+                    return nb::ready();
+                }
+                is_cr = byte == '\r';
+            }
+            return nb::pending;
         }
 
       public:
@@ -25,25 +94,34 @@ namespace nb::stream {
             }
 
             while (true) {
-                uint8_t writable_count = (index_ >= 1 && buffer_[index_ - 1] == '\r') ? 1 : 2;
-                uint8_t read_count = etl::min(source.readable_count(), writable_count);
-                if (read_count == 0) {
+                if (source.readable_count() == 0) {
                     return nb::pending;
                 }
 
-                source.read(etl::span(buffer_.data() + index_, read_count));
-                index_ += read_count;
-                if (is_complete()) {
-                    return nb::ready();
+                if (buffer_.full()) {
+                    POLL_UNWRAP_OR_RETURN(discard_line(source));
+                    buffer_.clear();
+                }
+
+                uint8_t readable_count = source.readable_count();
+                uint8_t writable_count = buffer_.available();
+                uint8_t count = etl::min(readable_count, writable_count);
+                for (int i = 0; i < count; i++) {
+                    buffer_.push_back(source.read());
+                    if (is_complete()) {
+                        return nb::ready();
+                    }
                 }
             }
         }
 
         nb::Poll<etl::span<const uint8_t>> poll() const {
-            if (is_complete()) {
-                return nb::ready(etl::span(buffer_.data(), index_));
-            }
-            return nb::pending;
+            return (is_complete()) ? nb::ready(etl::span(buffer_.begin(), buffer_.end()))
+                                   : nb::pending;
+        }
+
+        void reset() {
+            buffer_.clear();
         }
     };
 } // namespace nb::stream
--- test/test_nb/stream/single_line.cpp
+++ test/test_nb/stream/single_line.cpp
@@ -7,38 +7,88 @@
 using namespace nb::stream;
 using namespace util::u8_literal;
 
-TEST_CASE("empty buffer must return pending") {
-    SingleLineWritableBuffer<3> buffer;
-    auto poll = buffer.poll();
-    CHECK(poll.is_pending());
+TEST_CASE("SingleLineWritableBuffer") {
+    SUBCASE("empty buffer must return pending") {
+        SingleLineWritableBuffer<3> buffer;
+        auto poll = buffer.poll();
+        CHECK(poll.is_pending());
+    }
+
+    SUBCASE("buffer with \\r\\n must return ready") {
+        SingleLineWritableBuffer<3> buffer;
+        nb::stream::FixedReadableBuffer<2> source{'\r', '\n'};
+        auto poll = buffer.write_all_from(source);
+        CHECK(poll.is_ready());
+    }
+
+    SUBCASE("buffer with \\r must return pending") {
+        SingleLineWritableBuffer<3> buffer;
+        nb::stream::FixedReadableBuffer<1> source{'\r'};
+        auto poll = buffer.write_all_from(source);
+        CHECK(poll.is_pending());
+    }
+
+    SUBCASE("buffer with \\n must return pending") {
+        SingleLineWritableBuffer<3> buffer;
+        nb::stream::FixedReadableBuffer<1> source{'\n'};
+        auto poll = buffer.write_all_from(source);
+        CHECK(poll.is_pending());
+    }
+
+    SUBCASE("buffer contains \\r\\n must return ready") {
+        SingleLineWritableBuffer<14> buffer;
+        nb::stream::FixedReadableBuffer<14> source{"HELLO\r\nWORLD\r\n"_u8array};
+        buffer.write_all_from(source);
+        auto poll = buffer.poll();
+        CHECK(poll.is_ready());
+        CHECK(etl::equal(poll.unwrap(), etl::span("HELLO\r\n"_u8array)));
+    }
 }
 
-TEST_CASE("buffer with \\r\\n must return ready") {
-    SingleLineWritableBuffer<3> buffer;
-    nb::stream::FixedReadableBuffer<2> source{'\r', '\n'};
-    auto poll = buffer.write_all_from(source);
-    CHECK(poll.is_ready());
-}
+TEST_CASE("MaxLengthSingleLineWritableBuffer") {
+    SUBCASE("empty buffer must return pending") {
+        MaxLengthSingleLineWrtableBuffer<3> buffer;
+        auto poll = buffer.poll();
+        CHECK(poll.is_pending());
+    }
 
-TEST_CASE("buffer with \\r must return pending") {
-    SingleLineWritableBuffer<3> buffer;
-    nb::stream::FixedReadableBuffer<1> source{'\r'};
-    auto poll = buffer.write_all_from(source);
-    CHECK(poll.is_pending());
-}
+    SUBCASE("buffer with \\r\\n must return ready") {
+        MaxLengthSingleLineWrtableBuffer<3> buffer;
+        nb::stream::FixedReadableBuffer<2> source{'\r', '\n'};
+        auto poll = buffer.write_all_from(source);
+        CHECK(poll.is_ready());
+    }
 
-TEST_CASE("buffer with \\n must return pending") {
-    SingleLineWritableBuffer<3> buffer;
-    nb::stream::FixedReadableBuffer<1> source{'\n'};
-    auto poll = buffer.write_all_from(source);
-    CHECK(poll.is_pending());
-}
+    SUBCASE("buffer with \\r must return pending") {
+        MaxLengthSingleLineWrtableBuffer<3> buffer;
+        nb::stream::FixedReadableBuffer<1> source{'\r'};
+        auto poll = buffer.write_all_from(source);
+        CHECK(poll.is_pending());
+    }
 
-TEST_CASE("buffer contains \\r\\n must return ready") {
-    SingleLineWritableBuffer<14> buffer;
-    nb::stream::FixedReadableBuffer<14> source{"HELLO\r\nWORLD\r\n"_u8array};
-    buffer.write_all_from(source);
-    auto poll = buffer.poll();
-    CHECK(poll.is_ready());
-    CHECK(etl::equal(poll.unwrap(), etl::span("HELLO\r\n"_u8array)));
+    SUBCASE("buffer with \\n must return pending") {
+        MaxLengthSingleLineWrtableBuffer<3> buffer;
+        nb::stream::FixedReadableBuffer<1> source{'\n'};
+        auto poll = buffer.write_all_from(source);
+        CHECK(poll.is_pending());
+    }
+
+    SUBCASE("buffer contains \\r\\n must return ready") {
+        MaxLengthSingleLineWrtableBuffer<7> buffer;
+        nb::stream::FixedReadableBuffer<14> source{"HELLO\r\nWORLD\r\n"_u8array};
+        buffer.write_all_from(source);
+        auto poll = buffer.poll();
+        CHECK(poll.is_ready());
+        CHECK(etl::equal(poll.unwrap(), etl::span("HELLO\r\n"_u8array)));
+    }
+
+    SUBCASE("long buffer") {
+        MaxLengthSingleLineWrtableBuffer<5> buffer;
+        nb::stream::FixedReadableBuffer<14> source{"HELLO\r\nFOO\r\n"_u8array};
+        buffer.write_all_from(source);
+        buffer.write_all_from(source);
+        auto poll = buffer.poll();
+        CHECK(poll.is_ready());
+        CHECK(etl::equal(poll.unwrap(), etl::span("FOO\r\n"_u8array)));
+    }
 }
