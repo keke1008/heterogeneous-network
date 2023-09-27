a4572a82a7d8a26ed09aaf9684a258f42e3a765a
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-09-27 18:09:29 +0900

    feat: 旧データ送信プログラムを削除

--- lib/net/src/net/protocol/protocol_number.h
+++ lib/net/src/net/protocol/protocol_number.h
@@ -2,9 +2,6 @@
 
 #include <stdint.h>
 
-namespace net::protocol::protocol_number {
-    constexpr uint8_t TRANSMISSION_PRE = 0x01;
-    constexpr uint8_t TRANSMISSION_DATA = 0x02;
-    constexpr uint8_t TRANSMISSION_ACK = 0x03;
-    constexpr uint8_t TRANSMISSION_NACK = 0x04;
-} // namespace net::protocol::protocol_number
+namespace net::protocol {
+    enum class PROTOCOL_NUMBER : uint8_t {};
+} // namespace net::protocol
