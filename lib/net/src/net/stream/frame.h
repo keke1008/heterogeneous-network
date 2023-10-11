e0ffa9f6c56bc372907b8f298201eeeb8a5244b6
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-10-11 13:17:02 +0900

    refactor: チャネルを使用するように変更

--- lib/net/src/net/stream/frame.h
+++ lib/net/src/net/stream/frame.h
@@ -1,22 +1,38 @@
 #pragma once
 
 #include <debug_assert.h>
-#include <memory/pair_shared.h>
+#include <nb/channel.h>
 #include <nb/poll.h>
 #include <net/frame/service.h>
 
 namespace net::stream {
     class StreamWriter final : public nb::stream::WritableStream {
-        memory::Reference<etl::optional<frame::FrameBufferWriter>> writer_;
+        nb::OneBufferReceiver<frame::FrameBufferWriter> writer_rx_;
+        nb::OneBufferSender<frame::FrameBufferReader> written_reader_tx_;
 
-        inline const etl::optional<frame::FrameBufferWriter> &writer() const {
-            DEBUG_ASSERT(writer_.has_pair());
-            return writer_.get()->get();
+        inline const frame::FrameBufferWriter &unwrap_writer() const {
+            DEBUG_ASSERT(writer_rx_.is_receivable());
+            return writer_rx_.peek().unwrap().get();
         }
 
-        inline etl::optional<frame::FrameBufferWriter> &writer() {
-            DEBUG_ASSERT(writer_.has_pair());
-            return writer_.get()->get();
+        inline frame::FrameBufferWriter &unwrap_writer() {
+            DEBUG_ASSERT(writer_rx_.is_receivable());
+            return writer_rx_.peek().unwrap().get();
+        }
+
+        inline void replace_if_full() {
+            DEBUG_ASSERT(!writer_rx_.is_closed());
+            if (!writer_rx_.is_receivable()) {
+                return;
+            }
+
+            auto &writer = writer_rx_.peek().unwrap().get();
+            if (writer.writable_count() == 0) {
+                auto reader = writer.make_initial_reader();
+                if (written_reader_tx_.send(etl::move(reader)).is_ready()) {
+                    writer_rx_.receive();
+                }
+            }
         }
 
       public:
@@ -26,51 +42,59 @@ namespace net::stream {
         StreamWriter &operator=(const StreamWriter &) = delete;
         StreamWriter &operator=(StreamWriter &&) = delete;
 
-        inline ~StreamWriter() {
-            writer_.unpair();
-        }
-
-        explicit StreamWriter(memory::Reference<etl::optional<frame::FrameBufferWriter>> &&writer)
-            : writer_{etl::move(writer)} {}
+        explicit StreamWriter(
+            nb::OneBufferReceiver<frame::FrameBufferWriter> &&writer_rx,
+            nb::OneBufferSender<frame::FrameBufferReader> &&written_reader_tx
+        )
+            : writer_rx_{etl::move(writer_rx)},
+              written_reader_tx_{etl::move(written_reader_tx)} {}
 
         inline uint8_t writable_count() const override {
-            if (!writer_.has_pair()) { // 送信する相手がいない
-                return 255;            // 書き込まれたデータは破棄される
-            }
-            return writer().has_value() ? writer()->writable_count() : 0;
+            DEBUG_ASSERT(!writer_rx_.is_closed());
+            return writer_rx_.is_receivable() ? unwrap_writer().writable_count() : 0;
         }
 
         inline bool write(uint8_t byte) override {
-            if (!writer_.has_pair()) { // 送信する相手がいない
-                return false;          // 書き込まれたデータは破棄される
-            }
-            DEBUG_ASSERT(writer().has_value());
-            return writer()->write(byte);
+            DEBUG_ASSERT(writer_rx_.is_receivable());
+            bool result = unwrap_writer().write(byte);
+            replace_if_full();
+            return result;
         }
 
         inline bool write(etl::span<const uint8_t> buffer) override {
-            if (!writer_.has_pair()) { // 送信する相手がいない
-                return false;          // 書き込まれたデータは破棄される
-            }
-            return writer().has_value() && writer()->write(buffer);
+            DEBUG_ASSERT(!writer_rx_.is_closed());
+            bool result = writer_rx_.is_receivable() && unwrap_writer().write(buffer);
+            replace_if_full();
+            return result;
         }
 
         inline void close() {
-            writer_.unpair();
+            etl::move(writer_rx_).close();
+            etl::move(written_reader_tx_).close();
         }
     };
 
     class StreamReader final : public nb::stream::ReadableStream {
-        memory::Owned<etl::optional<frame::FrameBufferReader>> reader_;
+        nb::OneBufferReceiver<frame::FrameBufferReader> reader_rx_;
 
-        inline const etl::optional<frame::FrameBufferReader> &reader() const {
-            DEBUG_ASSERT(reader_.has_pair());
-            return reader_.get();
+        inline const frame::FrameBufferReader &reader() const {
+            DEBUG_ASSERT(reader_rx_.is_receivable());
+            return reader_rx_.peek().unwrap().get();
         }
 
-        inline etl::optional<frame::FrameBufferReader> &reader() {
-            DEBUG_ASSERT(reader_.has_pair());
-            return reader_.get();
+        inline frame::FrameBufferReader &reader() {
+            DEBUG_ASSERT(reader_rx_.is_receivable());
+            return reader_rx_.peek().unwrap().get();
+        }
+
+        inline void replace_if_full() {
+            if (!reader_rx_.is_receivable()) {
+                return;
+            }
+
+            if (reader().readable_count() == 0) {
+                reader_rx_.receive();
+            }
         }
 
       public:
@@ -80,25 +104,28 @@ namespace net::stream {
         StreamReader &operator=(const StreamReader &) = delete;
         StreamReader &operator=(StreamReader &&) = delete;
 
-        explicit StreamReader(memory::Owned<etl::optional<frame::FrameBufferReader>> reader)
-            : reader_{etl::move(reader)} {}
+        explicit StreamReader(nb::OneBufferReceiver<frame::FrameBufferReader> &&reader_rx)
+            : reader_rx_{etl::move(reader_rx)} {}
 
         inline uint8_t readable_count() const override {
-            return reader().has_value() ? reader()->readable_count() : 0;
+            return reader_rx_.is_receivable() ? reader().readable_count() : 0;
         }
 
         inline uint8_t read() override {
-            DEBUG_ASSERT(reader().has_value());
-            return reader()->read();
+            DEBUG_ASSERT(reader_rx_.is_receivable());
+            uint8_t byte = reader().read();
+            replace_if_full();
+            return byte;
         }
 
         inline void read(etl::span<uint8_t> buffer) override {
-            DEBUG_ASSERT(reader().has_value());
-            return reader()->read(buffer);
+            DEBUG_ASSERT(reader_rx_.is_receivable());
+            reader().read(buffer);
+            replace_if_full();
         }
 
         inline bool is_closed() const {
-            return !reader_.has_pair();
+            return reader_rx_.is_closed();
         }
     };
 } // namespace net::stream
---
+++ lib/net/src/net/stream/socket.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "./frame.h"
+#include <nb/channel.h>
+#include <net/frame/service.h>
+#include <stdint.h>
+#include <util/concepts.h>
+
+namespace net::stream {
+    class StreamSenderSocket {
+        nb::OneBufferSender<frame::FrameBufferWriter> writer_tx_;
+        nb::OneBufferReceiver<frame::FrameBufferReader> written_reader_rx_;
+
+      public:
+        StreamSenderSocket() = delete;
+        StreamSenderSocket(const StreamSenderSocket &) = delete;
+        StreamSenderSocket(StreamSenderSocket &&) = default;
+        StreamSenderSocket &operator=(const StreamSenderSocket &) = delete;
+        StreamSenderSocket &operator=(StreamSenderSocket &&) = delete;
+
+        explicit StreamSenderSocket(
+            nb::OneBufferSender<frame::FrameBufferWriter> &&writer_tx,
+            nb::OneBufferReceiver<frame::FrameBufferReader> &&reader_rx
+        )
+            : writer_tx_{etl::move(writer_tx)},
+              written_reader_rx_{etl::move(reader_rx)} {}
+
+        inline bool is_closed() {
+            return written_reader_rx_.is_closed();
+        }
+
+        // 送信されていないデータがある場合は`nb::Pending`を返す
+        template <frame::IFrameBufferRequester Requester, frame::IFrameSender Sender>
+        nb::Poll<void> execute(Requester &requester, Sender &sender) {
+            if (writer_tx_.is_closed()) {
+                return nb::ready();
+            }
+
+            auto reader = POLL_UNWRAP_OR_RETURN(written_reader_rx_.peek());
+            POLL_UNWRAP_OR_RETURN(sender.send_frame(reader.get()));
+            written_reader_rx_.receive();
+
+            if (writer_tx_.is_sendable()) {
+                auto writer = POLL_UNWRAP_OR_RETURN(requester.request_max_length_frame_writer());
+                writer_tx_.send(etl::move(writer));
+            }
+
+            return nb::pending;
+        }
+    };
+
+    static etl::pair<StreamSenderSocket, StreamWriter> make_send_stream() {
+        auto [tx1, rx1] = nb::make_one_buffer_channel<frame::FrameBufferWriter>();
+        auto [tx2, rx2] = nb::make_one_buffer_channel<frame::FrameBufferReader>();
+        return {
+            StreamSenderSocket{etl::move(tx1), etl::move(rx2)},
+            StreamWriter{etl::move(rx1), etl::move(tx2)},
+        };
+    }
+
+    class StreamReceiverSocket {
+        nb::OneBufferSender<frame::FrameBufferReader> reader_tx_;
+
+      public:
+        StreamReceiverSocket() = delete;
+        StreamReceiverSocket(const StreamReceiverSocket &) = delete;
+        StreamReceiverSocket(StreamReceiverSocket &&) = default;
+        StreamReceiverSocket &operator=(const StreamReceiverSocket &) = delete;
+        StreamReceiverSocket &operator=(StreamReceiverSocket &&) = delete;
+
+        explicit StreamReceiverSocket(nb::OneBufferSender<frame::FrameBufferReader> &&reader_tx_)
+            : reader_tx_{etl::move(reader_tx_)} {}
+
+        template <frame::IFrameReceiver Receiver>
+        nb::Poll<void> execute(Receiver &receiver) {
+            if (reader_tx_.is_closed()) { // 受信したデータを扱う相手がいない
+                while (receiver.receive_frame().is_ready()) {} // 受信したデータを捨てる
+                return nb::ready();
+            }
+
+            POLL_UNWRAP_OR_RETURN(reader_tx_.poll_sendable());
+            reader_tx_.send(POLL_UNWRAP_OR_RETURN(receiver.receive_frame()));
+        }
+
+        void close() {
+            etl::move(reader_tx_).close();
+        }
+    };
+
+    static etl::pair<StreamReceiverSocket, StreamReader> make_receive_stream() {
+        auto [tx, rx] = nb::make_one_buffer_channel<frame::FrameBufferReader>();
+        return {StreamReceiverSocket{etl::move(tx)}, StreamReader{etl::move(rx)}};
+    }
+} // namespace net::stream
