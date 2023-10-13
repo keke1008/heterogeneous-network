276b18f189573f61be66177d2944008536496745
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-10-13 16:12:50 +0900

    feat: リンクステート型のルーティングプロトコルを追加

--- lib/net/src/net/routing/link_state/table.h
+++ lib/net/src/net/routing/link_state/table.h
@@ -1,169 +1,265 @@
 #pragma once
 
-#include "../node.h"
-#include "./resolver.h"
-#include <etl/algorithm.h>
-#include <nb/buf/builder.h>
-#include <nb/buf/splitter.h>
+#include "./node.h"
+#include <etl/priority_queue.h>
+#include <etl/stack.h>
+#include <nb/buf.h>
 #include <net/link.h>
+#include <net/stream/socket.h>
 
 namespace net::routing::link_state {
-    constexpr uint8_t MAX_NODES = 20;
-    constexpr uint8_t MAX_EDGES = 40;
+    constexpr uint8_t MAX_NODES = 30;
+    constexpr uint8_t MAX_PEERS = 10;
 
     using NodeIndex = uint8_t;
 
-    class LinkStateRoutingTable final : nb::buf::BufferWriter {
-        resolver::AdjacencyCost<MAX_NODES> cost_;
-        etl::array<etl::optional<NodeAddress>, MAX_NODES> cost_index_to_node_;
-        NodeIndex self_index_;
+    struct Peer {
+        NodeIndex id;
+        Cost cost;
+    };
+
+    class Peers {
+        uint8_t count_ = 0;
+        etl::array<Peer, MAX_PEERS> peers_;
 
       public:
-        explicit LinkStateRoutingTable(const NodeAddress &self_address) {
-            cost_index_to_node_[0] = self_address;
-            cost_.set_vertex(0, Cost{0});
-            self_index_ = 0;
+        Peers() = default;
+
+        inline bool empty() const {
+            return count_ == 0;
         }
 
-        etl::optional<NodeIndex> get_node(const NodeAddress &address) const {
-            for (uint8_t i = 0; i < cost_index_to_node_.size(); i++) {
-                if (cost_index_to_node_[i] == address) {
+        inline uint8_t size() const {
+            return count_;
+        }
+
+        inline Peer &at(uint8_t index) {
+            return peers_[index];
+        }
+
+        inline bool contains(NodeIndex id) const {
+            for (uint8_t i = 0; i < count_; i++) {
+                if (peers_[i].id == id) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        inline bool insert(NodeIndex id, Cost cost) {
+            if (count_ == peers_.size()) {
+                return false;
+            }
+
+            peers_[count_++] = Peer{id, cost};
+            return true;
+        }
+
+        inline const Peer *begin() const {
+            return peers_.begin();
+        }
+
+        inline Peer *begin() {
+            return peers_.begin();
+        }
+
+        inline const Peer *end() const {
+            return peers_.begin() + count_;
+        }
+
+        inline Peer *end() {
+            return peers_.begin() + count_;
+        }
+
+        inline void remove(NodeIndex id) {
+            for (uint8_t i = 0; i < count_; i++) {
+                if (peers_[i].id == id) {
+                    peers_[i] = peers_[--count_];
+                    return;
+                }
+            }
+        }
+    };
+
+    struct Node {
+        NodeId id;
+        Cost cost;
+        Peers peers;
+
+        inline Node(NodeId id, Cost cost) : id{id}, cost{cost} {}
+
+        inline bool has_peer() const {
+            return !peers.empty();
+        }
+    };
+
+    class Table {
+        static constexpr uint8_t SELF_INDEX = 0;
+        etl::vector<etl::optional<Node>, MAX_NODES> nodes_;
+
+        void remove_isolated_nodes() {
+            etl::array<bool, MAX_NODES> found;
+            found.fill(false);
+            found[SELF_INDEX] = true;
+
+            etl::stack<NodeIndex, MAX_NODES> stack;
+            stack.push(SELF_INDEX);
+            while (!stack.empty()) {
+                auto node_index = stack.top();
+                stack.pop();
+
+                for (auto peer : nodes_[node_index].value().peers) {
+                    if (!found[peer.id]) {
+                        found[peer.id] = true;
+                        stack.push(peer.id);
+                    }
+                }
+            }
+
+            for (uint8_t i = 0; i < nodes_.size(); i++) {
+                if (!found[i]) {
+                    nodes_[i] = etl::nullopt;
+                }
+            }
+        }
+
+        etl::optional<NodeIndex> insert_node(const NodeId &id, Cost cost) {
+            for (uint8_t i = 0; i < nodes_.size(); i++) {
+                if (!nodes_[i].has_value()) {
+                    nodes_[i].emplace(id, cost);
+                    return i;
+                }
+            }
+
+            return etl::nullopt;
+        }
+
+      public:
+        explicit Table(const NodeId &self_id, Cost self_cost) {
+            nodes_.emplace_back(self_id, self_cost);
+        }
+
+        inline constexpr uint8_t max_node_count() const {
+            return nodes_.size();
+        }
+
+        inline const etl::optional<Node> &resolve_node(NodeIndex index) const {
+            return nodes_[index];
+        }
+
+        etl::optional<NodeIndex> get_node(const NodeId &id) const {
+            for (uint8_t i = 0; i < nodes_.size(); i++) {
+                if (nodes_[i].has_value() && nodes_[i].value().id == id) {
                     return i;
                 }
             }
             return etl::nullopt;
         }
 
-        etl::optional<NodeIndex> insert_node(const NodeAddress &address, Cost cost) {
-            for (uint8_t i = 0; i < cost_index_to_node_.size(); i++) {
-                if (!cost_index_to_node_[i].has_value()) {
-                    cost_index_to_node_[i] = address;
-                    cost_.set_vertex(i, cost);
-                    return i;
+        etl::optional<NodeIndex> get_or_insert_node(const NodeId &id, Cost cost) {
+            auto node_index = get_node(id);
+            if (node_index.has_value()) {
+                return node_index;
+            }
+
+            auto inserted_index = insert_node(id, cost);
+            if (inserted_index.has_value()) {
+                return inserted_index;
+            }
+
+            remove_isolated_nodes();
+            return insert_node(id, cost);
+        }
+
+        bool insert_edge(NodeIndex node1, NodeIndex node2, Cost cost) {
+            auto n1 = nodes_[node1].value();
+            auto n2 = nodes_[node2].value();
+
+            if (!n1.peers.contains(node2) && n1.peers.insert(node2, cost)) {
+                n2.peers.insert(node1, cost);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        void remove_node(NodeIndex node_index) {
+            auto &peers = nodes_[node_index].value().peers;
+            for (auto peer : peers) {
+                auto &peer_node = nodes_[peer.id].value();
+                peer_node.peers.remove(node_index);
+            }
+            nodes_[node_index] = etl::nullopt;
+        }
+
+        struct Route {
+            uint16_t cost;
+            NodeIndex gateway;
+            NodeIndex peer;
+        };
+
+        etl::optional<Route> resolve_route(NodeIndex destination) const {
+            struct Vertex {
+                uint16_t cost;
+                NodeIndex node;
+                NodeIndex gateway;
+                NodeIndex peer;
+
+                inline constexpr bool operator<(const Vertex &other) const {
+                    return cost < other.cost;
+                }
+            };
+
+            etl::priority_queue<
+                Vertex, MAX_NODES, etl::vector<Vertex, MAX_NODES>, etl::greater<Vertex>>
+                queue;
+            queue.push({
+                .cost = 0,
+                .node = SELF_INDEX,
+                .gateway = SELF_INDEX,
+                .peer = SELF_INDEX,
+            });
+
+            etl::array<bool, MAX_NODES> visited;
+            visited.fill(false);
+            visited[SELF_INDEX] = true;
+            etl::array<uint16_t, MAX_NODES> min_cost;
+            min_cost.fill(etl::numeric_limits<uint16_t>::max());
+            min_cost[SELF_INDEX] = 0;
+
+            while (!queue.empty()) {
+                Vertex v = queue.top();
+                queue.pop();
+
+                if (visited[v.node] && v.cost > min_cost[v.node]) {
+                    continue;
+                }
+                visited[v.node] = true;
+
+                if (v.node == destination) {
+                    return Route{.cost = v.cost, .gateway = v.gateway, .peer = v.peer};
+                }
+
+                auto &node = nodes_[v.node].value();
+                for (const auto &peer : node.peers) {
+                    if (visited[peer.id]) {
+                        continue;
+                    }
+
+                    uint16_t new_cost = v.cost + node.cost.value() + peer.cost.value();
+                    if (new_cost < min_cost[peer.id]) {
+                        min_cost[peer.id] = new_cost;
+                        queue.push({
+                            .cost = new_cost,
+                            .node = peer.id,
+                            .gateway = v.gateway,
+                            .peer = v.node,
+                        });
+                    }
                 }
             }
 
             return etl::nullopt;
-        }
-
-        inline bool insert_edge(NodeIndex source, NodeIndex destination, Cost cost) {
-            return cost_.set(source, destination, cost);
-        }
-
-        inline void remove_node(NodeIndex node_index) {
-            cost_index_to_node_[node_index] = etl::nullopt;
-            cost_.remove_vertex(node_index);
-        }
-
-        etl::optional<NodeAddress> resolve_gateway(const NodeAddress &dest) const {
-            auto dest_node_index = get_node(dest);
-            if (!dest_node_index.has_value()) {
-                return etl::nullopt;
-            }
-
-            etl::array<bool, MAX_NODES> valid_nodes;
-            for (uint8_t i = 0; i < valid_nodes.size(); i++) {
-                valid_nodes[i] = cost_index_to_node_[i].has_value();
-            }
-            auto gateway_index = resolver::resolve_gateway_vertex<MAX_NODES>(
-                cost_, valid_nodes, self_index_, dest_node_index.value()
-            );
-            if (!gateway_index.has_value()) {
-                return etl::nullopt;
-            }
-
-            return cost_index_to_node_[gateway_index.value()];
-        }
-
-        bool merge_from_frame(nb::buf::BufferSplitter &splitter) {
-            bool changed = false;
-            static constexpr NodeIndex NODE_FULL = etl::numeric_limits<NodeIndex>::max();
-            etl::array<NodeIndex, MAX_NODES> packet_to_table;
-
-            uint8_t node_count = splitter.split_1byte();
-            for (uint8_t i = 0; i < node_count; i++) {
-                NodeAddress node_address = splitter.parse<NodeAddressParser>();
-                Cost cost{splitter.split_1byte()};
-
-                auto node_index = get_node(node_address);
-                if (node_index.has_value()) {
-                    packet_to_table[i] = node_index.value();
-                    continue;
-                }
-
-                auto new_node_index = insert_node(node_address, cost);
-                if (new_node_index.has_value()) {
-                    packet_to_table[i] = new_node_index.value();
-                    changed = true;
-                } else {
-                    packet_to_table[i] = NODE_FULL;
-                }
-            }
-
-            uint8_t edge_count = splitter.split_1byte();
-            for (uint8_t i = 0; i < edge_count; i++) {
-                NodeIndex source = packet_to_table[splitter.split_1byte()];
-                NodeIndex destination = packet_to_table[splitter.split_1byte()];
-                Cost cost{splitter.split_1byte()};
-
-                if (source != NODE_FULL && destination != NODE_FULL) {
-                    changed |= insert_edge(source, destination, cost);
-                }
-            }
-
-            return changed;
-        }
-
-        void write_to_builder(nb::buf::BufferBuilder &builder) override {
-            uint8_t node_count = etl::count_if(
-                cost_index_to_node_.begin(), cost_index_to_node_.end(),
-                [](const auto &node) { return node.has_value(); }
-            );
-            builder.append(node_count);
-
-            for (uint8_t i = 0; i < cost_index_to_node_.size(); i++) {
-                if (!cost_index_to_node_[i].has_value()) {
-                    continue;
-                }
-
-                builder.append(cost_index_to_node_[i].value());
-                builder.append(cost_.get_vertex(i).value());
-            }
-
-            uint8_t edge_count = 0;
-            for (uint8_t i = 0; i < cost_index_to_node_.size(); i++) {
-                if (!cost_index_to_node_[i].has_value()) {
-                    continue;
-                }
-                for (uint8_t j = i + 1; j < cost_index_to_node_.size(); j++) {
-                    if (!cost_index_to_node_[j].has_value()) {
-                        continue;
-                    }
-
-                    if (!cost_.get(i, j).is_infinite()) {
-                        edge_count++;
-                    }
-                }
-            }
-            builder.append(edge_count);
-
-            for (uint8_t i = 0; i < cost_index_to_node_.size(); i++) {
-                if (!cost_index_to_node_[i].has_value()) {
-                    continue;
-                }
-                for (uint8_t j = i + 1; j < cost_index_to_node_.size(); j++) {
-                    if (!cost_index_to_node_[j].has_value()) {
-                        continue;
-                    }
-
-                    Cost cost = cost_.get(i, j);
-                    if (!cost.is_infinite()) {
-                        builder.append(i);
-                        builder.append(j);
-                        builder.append(cost.value());
-                    }
-                }
-            }
         }
     };
 } // namespace net::routing::link_state
