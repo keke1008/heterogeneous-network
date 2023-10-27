bfb038e2156beb51f972a7ad316a3c6e4342300e
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-10-27 10:38:23 +0900

    feat: emulatorワークスペースの作成

---
+++ emulator/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1>counter</h1>
+            <div className="card">
+                <button>count is {0}</button>
+            </div>
+        </>
+    );
+}
+
+export default App;
