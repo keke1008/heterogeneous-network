8336625ec7f42c60327a7d2e8c6dfd4a38ba686c
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-10-28 13:50:15 +0900

    fix(emulator): ビルド環境の修正

--- emulator/electron/electron-env.d.ts
+++ emulator/electron/electron-env.d.ts
@@ -1,25 +1,35 @@
 /// <reference types="vite-plugin-electron/electron-env" />
+import { Graph } from "./net";
 
-declare namespace NodeJS {
-    interface ProcessEnv {
-        /**
-         * The built directory structure
-         *
-         * ```tree
-         * ├─┬─┬ dist
-         * │ │ └── index.html
-         * │ │
-         * │ ├─┬ dist-electron
-         * │ │ ├── main.js
-         * │ │ └── preload.js
-         * │
-         * ```
-         */
-        DIST: string;
-        /** /dist/ or /public/ */
-        VITE_PUBLIC: string;
+declare global {
+    declare namespace NodeJS {
+        interface ProcessEnv {
+            /**
+             * The built directory structure
+             *
+             * ```tree
+             * ├─┬─┬ dist
+             * │ │ └── index.html
+             * │ │
+             * │ ├─┬ dist-electron
+             * │ │ ├── main.js
+             * │ │ └── preload.js
+             * │
+             * ```
+             */
+            DIST: string;
+            /** /dist/ or /public/ */
+            VITE_PUBLIC: string;
+        }
+    }
+
+    // Used in Renderer process, expose in `preload.ts`
+    interface Window {
+        net: {
+            begin: (args: { selfAddress: string; selfPort: string }) => void;
+            onGraphChanged: (callback: (graph: Graph) => void) => void;
+            connect: (args: { address: string; port: string; cost: number }) => void;
+            end: () => void;
+        };
     }
 }
-
-// Used in Renderer process, expose in `preload.ts`
-interface Window {}
--- emulator/vite.config.ts
+++ emulator/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "node:path";
 import electron from "vite-plugin-electron/simple";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
             main: {
                 // Shortcut of `build.lib.entry`.
                 entry: "electron/main.ts",
+                vite: { plugins: [tsconfigPaths()] },
             },
             preload: {
                 // Shortcut of `build.rollupOptions.input`.
