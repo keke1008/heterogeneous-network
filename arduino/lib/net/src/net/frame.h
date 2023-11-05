61c2aa472818bb3b4bad5611948fcbf78f3da0c1
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-11-05 12:19:41 +0900

    feat(arduino): frame_idを作成

--- arduino/lib/net/src/net/frame.h
+++ arduino/lib/net/src/net/frame.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #include "./frame/buffer.h"
+#include "./frame/frame_id.h"
 #include "./frame/protocol_number.h"
 #include "./frame/service.h"
---
+++ arduino/lib/net/src/net/frame/frame_id.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <etl/circular_buffer.h>
+#include <nb/buf.h>
+#include <stdint.h>
+#include <util/rand.h>
+
+namespace net::frame {
+    class FrameId {
+        uint16_t id_;
+
+      public:
+        explicit FrameId(uint16_t id) : id_(id) {}
+
+        static FrameId random(util::Rand &rand) {
+            return FrameId{rand.gen_uint16_t()};
+        }
+
+        inline uint16_t get() const {
+            return id_;
+        }
+
+        inline bool operator==(const FrameId &other) const {
+            return id_ == other.id_;
+        }
+
+        inline bool operator!=(const FrameId &other) const {
+            return id_ != other.id_;
+        }
+
+        inline static FrameId parse(nb::buf::BufferSplitter &splitter) {
+            return FrameId{splitter.parse<nb::buf::BinParser<uint16_t>>()};
+        }
+
+        inline uint8_t serialized_length() const {
+            return sizeof(id_);
+        }
+
+        inline void write_to_builder(nb::buf::BufferBuilder &builder) const {
+            builder.append(nb::buf::FormatBinary<uint16_t>(id_));
+        }
+    };
+
+    template <uint8_t MAX_CACHE_SIZE>
+    class FrameIdCache {
+        etl::circular_buffer<FrameId, MAX_CACHE_SIZE> cache_{};
+
+      public:
+        FrameIdCache() = default;
+
+        inline void add(FrameId id) {
+            cache_.push_back(id);
+        }
+
+        inline bool contains(FrameId id) const {
+            return etl::any_of(cache_.begin(), cache_.end(), [id](FrameId other) {
+                return id == other;
+            });
+        }
+
+        inline FrameId generate(util::Rand &rand) {
+            FrameId id = FrameId::random(rand);
+            while (contains(id)) {
+                id = FrameId::random(rand);
+            }
+            add(id);
+            return id;
+        }
+    };
+} // namespace net::frame
