9ffa0179e3b3b91bd680d9d4ac1669951c478f7b
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-11-09 20:30:05 +0900

    feat(arduino): Initialize用のスクリプトを作成

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -10,9 +10,16 @@ net::App app{time};
 void setup() {
     util::set_error_handler();
 
+    Serial.begin(9600);
+    Serial1.begin(9600);
+    Serial2.begin(9600);
+    Serial3.begin(9600);
+
     app.add_serial_port(time, Serial1);
     app.add_serial_port(time, Serial2);
     app.add_serial_port(time, Serial3);
+
+    Serial.println("Setup complete");
 }
 
 void loop() {
