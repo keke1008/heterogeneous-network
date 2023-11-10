0cde88e4c1e9f79624bdcf05559520aa954587ad
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-11-10 21:36:49 +0900

    feat(arduino): FlashStringと`const char*`を混同しないように変更

--- arduino/lib/util/src/util/flash_string.h
+++ arduino/lib/util/src/util/flash_string.h
@@ -1,21 +1,33 @@
 #pragma once
 
+#include <etl/type_traits.h>
+#include <stdint.h>
+
 #if __has_include(<Arduino.h>)
 #include <Arduino.h>
 
-#define FLASH_STRING(string_literal)                                                               \
-    (reinterpret_cast<const __FlashStringHelper *>(PSTR(string_literal)))
-
 namespace util {
     using FlashStringType = const __FlashStringHelper *;
-}
+
+    inline uint8_t read_flash_byte(FlashStringType flash_string) {
+        return pgm_read_byte(flash_string);
+    }
+
+#define FLASH_STRING(string_literal) (reinterpret_cast<util::FlashStringType>(PSTR(string_literal)))
+
+} // namespace util
 
 #else
 
-#define FLASH_STRING(string_literal) string_literal
-
 namespace util {
-    using FlashStringType = const char *;
-}
+    class __FlashStringHelper;
+    using FlashStringType = const __FlashStringHelper *;
+
+    inline uint8_t read_flash_byte(FlashStringType flash_string) {
+        return *reinterpret_cast<const uint8_t *>(flash_string);
+    }
+} // namespace util
+
+#define FLASH_STRING(string_literal) (reinterpret_cast<util::FlashStringType>(string_literal))
 
 #endif
