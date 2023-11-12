63a3cbe5b15cf90a6189678de570c1c2ea465db4
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-11-12 22:43:18 +0900

    fix(emulator): 環境変数でmain.tsがエラーを吐かないように変更

--- emulator/electron/electron-env.d.ts
+++ emulator/electron/electron-env.d.ts
@@ -1,24 +1,22 @@
 /// <reference types="vite-plugin-electron/electron-env" />
 
-declare global {
-    declare namespace NodeJS {
-        interface ProcessEnv {
-            /**
-             * The built directory structure
-             *
-             * ```tree
-             * ├─┬─┬ dist
-             * │ │ └── index.html
-             * │ │
-             * │ ├─┬ dist-electron
-             * │ │ ├── main.js
-             * │ │ └── preload.js
-             * │
-             * ```
-             */
-            DIST: string;
-            /** /dist/ or /public/ */
-            VITE_PUBLIC: string;
-        }
+declare namespace NodeJS {
+    interface ProcessEnv {
+        /**
+         * The built directory structure
+         *
+         * ```tree
+         * ├─┬─┬ dist
+         * │ │ └── index.html
+         * │ │
+         * │ ├─┬ dist-electron
+         * │ │ ├── main.js
+         * │ │ └── preload.js
+         * │
+         * ```
+         */
+        DIST: string;
+        /** /dist/ or /public/ */
+        VITE_PUBLIC: string;
     }
 }
