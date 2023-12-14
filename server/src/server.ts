1ad3194041947fb85243fb211084608ef65baf78
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-12-14 11:06:27 +0900

    feat(server): 同プロセスでサーバを動かせるように修正

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,9 @@ import { UdpHandler } from "@core/media/dgram";
 import { WebSocketHandler } from "./websocket";
 import * as Rpc from "./rpc";
 import { VRouterService } from "./vrouter";
-import { IpAddressWithPrefix } from "./command/ip";
 
 const UDP_SERVER_LISTEN_PORT = 12345;
 const WEBSOCKET_SERVER_LISTEN_PORT = 12346;
-const ROOT_BRIDGE_LOCAL_ADDRESS = IpAddressWithPrefix.schema.parse("10.0.0.254/24");
-const VROUTER_LOCAL_ADDRESS_RANGE = IpAddressWithPrefix.schema.parse("10.0.0.0/24");
 
 const main = async (): Promise<void> => {
     console.log("Initializing...");
@@ -20,10 +17,7 @@ const main = async (): Promise<void> => {
     const webSocketHandler = new WebSocketHandler({ port: WEBSOCKET_SERVER_LISTEN_PORT });
     net.addHandler(AddressType.WebSocket, webSocketHandler);
 
-    const vRouterService = await VRouterService.create({
-        rootBridgeLocalAddress: ROOT_BRIDGE_LOCAL_ADDRESS,
-        vRouterLocalAddreessRange: VROUTER_LOCAL_ADDRESS_RANGE,
-    });
+    const vRouterService = new VRouterService();
     const rpc = net.rpc();
     rpc.addServer(Procedure.GetVRouters, new Rpc.GetVRoutersServer({ vRouterService }));
     rpc.addServer(Procedure.CreateVRouter, new Rpc.CreateVRouterServer({ vRouterService }));
--- server/src/vrouter/index.ts
+++ server/src/vrouter/index.ts
@@ -1,74 +1,49 @@
-import * as path from "node:path";
-import { fileURLToPath } from "node:url";
-import { spawn, ChildProcess } from "node:child_process";
-import { NetNsManager } from "./netns";
-import { VRouterInterfaceStore } from "./interface";
-import { ObjectMap } from "@core/object";
+import { AddressType, NetFacade, NetFacadeBuilder } from "@core/net";
+import { UdpHandler } from "@core/media/dgram";
 import { Port } from "../command/nftables";
-import { IpAddressWithPrefix } from "../command/ip";
 
-const SCRIPT_PATH = path.join(path.dirname(fileURLToPath(import.meta.url)), "process.ts");
-const TYPESCRIPT_EXECUTABLE = "tsx";
+class VRouterHandle {
+    #net: NetFacade = new NetFacadeBuilder().buildWithDefaults();
+    #udpHandler: UdpHandler;
 
-export class VRouterHandle {
-    #controller: AbortController;
-    #child: ChildProcess;
-
-    constructor(args: { controller: AbortController; child: ChildProcess }) {
-        this.#controller = args.controller;
-        this.#child = args.child;
+    constructor(port: number) {
+        this.#udpHandler = new UdpHandler(port);
+        this.#net.addHandler(AddressType.Udp, this.#udpHandler);
     }
 
-    kill(): void {
-        this.#controller.abort();
+    onClose(callback: () => void) {
+        this.#udpHandler.onClose(callback);
     }
 
-    onExit(callback: () => void): void {
-        this.#child.on("exit", callback);
+    close() {
+        this.#net.dispose();
+        this.#udpHandler.close();
     }
 }
 
 export class VRouterService {
-    #network: NetNsManager;
-    #interfaces = new VRouterInterfaceStore();
-    #controllers = new ObjectMap<Port, AbortController, number>((port) => port.toNumber());
-
-    private constructor(args: { network: NetNsManager }) {
-        this.#network = args.network;
-    }
-
-    static async create(args: {
-        vRouterLocalAddreessRange: IpAddressWithPrefix;
-        rootBridgeLocalAddress: IpAddressWithPrefix;
-    }) {
-        return new VRouterService({ network: await NetNsManager.create(args) });
-    }
+    #ports: number[] = [10001, 10002, 10003, 10004, 10005];
+    #handles = new Map<number, VRouterHandle>();
 
     async spawn(): Promise<Port | undefined> {
-        const interface_ = this.#interfaces.popNext();
-        if (interface_ === undefined) {
+        const port = this.#ports.shift();
+        if (port === undefined) {
             return;
         }
 
-        const network = await this.#network.createVRouter(interface_);
-        const controller = new AbortController();
-        const child = spawn(TYPESCRIPT_EXECUTABLE, [SCRIPT_PATH], { signal: controller.signal });
-        child.on("exit", () => {
-            this.#controllers.delete(interface_.globalPort);
-            this.#network.deleteVRouter(network);
-            this.#interfaces.pushBack(interface_);
-        });
-
-        return interface_.globalPort;
+        const handle = new VRouterHandle(port);
+        handle.onClose(() => this.#ports.push(port));
+        this.#handles.set(port, handle);
+        return Port.schema.parse(port);
     }
 
     kill(port: Port): boolean {
-        const controller = this.#controllers.get(port);
-        controller?.abort();
-        return controller !== undefined;
+        const handle = this.#handles.get(port.toNumber());
+        handle?.close();
+        return this.#handles.delete(port.toNumber());
     }
 
     getPorts(): Port[] {
-        return Array.from(this.#controllers.keys());
+        return Array.from(this.#handles.keys()).map((port) => Port.schema.parse(port));
     }
 }
