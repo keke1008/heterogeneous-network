83e9ad0a1d1f77937266b050723bc18d81d58ac6
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-12-20 11:15:24 +0900

    fix(arduino): ESP8266の初期設定の変更

--- arduino/lib/net/src/net/link/wifi/control/initialization.cpp
+++ arduino/lib/net/src/net/link/wifi/control/initialization.cpp
@@ -2,7 +2,7 @@
 
 namespace net::link::wifi {
     const etl::array<etl::string_view, 3> COMMANDS = {
-        "ATE0\r\n",          // Disable echo
+        "AT+CIPMUX=0\r\n",   // Disable multiple connections
         "AT+CWMODE=1\r\n",   // Set station mode
         "AT+CIPDINFO=1\r\n", // Show connection info
     };
