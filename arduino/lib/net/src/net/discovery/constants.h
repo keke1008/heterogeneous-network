4788001f2f7f67d41203f1512649cf484133e139
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-12-24 18:05:45 +0900

    feat(arduino): Discoveryの定数の調整

--- arduino/lib/net/src/net/discovery/constants.h
+++ arduino/lib/net/src/net/discovery/constants.h
@@ -8,8 +8,8 @@ namespace net::discovery {
     constexpr inline util::Duration DISCOVERY_FIRST_RESPONSE_TIMEOUT =
         util::Duration::from_millis(1000);
     constexpr inline uint8_t DISCOVERY_BETTER_RESPONSE_TIMEOUT_RATE = 1;
-    constexpr inline util::Duration DISCOVER_INTERVAL = util::Duration::from_millis(100);
+    constexpr inline util::Duration DISCOVER_INTERVAL = util::Duration::from_millis(25);
     constexpr inline uint8_t MAX_CONCURRENT_DISCOVERIES = 4;
-    constexpr inline uint8_t MAX_DISCOVERY_CACHE_ENTRIES = 8;
+    constexpr inline uint8_t MAX_DISCOVERY_CACHE_ENTRIES = 4;
     constexpr inline uint8_t FRAME_ID_CACHE_SIZE = 8;
 } // namespace net::discovery
