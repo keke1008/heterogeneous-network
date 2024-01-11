e6dfe870c78b71bc64f1b718b536d7bc1dd2b1f2
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-01-11 17:00:16 +0900

    feat(caption): ログ追加

--- caption-server/src/main.py
+++ caption-server/src/main.py
@@ -1,6 +1,15 @@
 from http.server import HTTPServer
 from server import CaptionServerHandler
+import logging
 
 if __name__ == "__main__":
-    server = HTTPServer(("localhost", 8080), CaptionServerHandler)
-    server.serve_forever()
+    host, port = "", 8080
+    server = HTTPServer((host, port), CaptionServerHandler)
+
+    logging.info(f"Server started on port {port}")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    server.server_close()
+    logging.info(f"Server stopped")
