298ceedbd03003605941c1ccf31e995238e329a5
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-01-13 13:28:27 +0900

    feat(arduino): 通知が満杯だった場合にログを出力するように変更

--- arduino/lib/net/src/net/notification.h
+++ arduino/lib/net/src/net/notification.h
@@ -33,7 +33,9 @@ namespace net::notification {
 
       public:
         inline void notify(const LocalNotification &notification) {
-            if (!notification_buffer_.full()) {
+            if (notification_buffer_.full()) {
+                LOG_INFO(FLASH_STRING("Notification buffer is full"));
+            } else {
                 notification_buffer_.push(notification);
             }
         }
