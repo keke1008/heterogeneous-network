892191524fcf8385791c3d49959a9b9b91521dc2
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-01-21 00:25:38 +0900

    feat(ts-core): Udpでグローバルアドレスを取得するプロトコルを削除

--- ts-core/src/media/dgram.ts
+++ ts-core/src/media/dgram.ts
@@ -13,15 +13,7 @@ import {
     ProtocolSerdeable,
     UdpAddress,
 } from "@core/net";
-import {
-    ConstantSerdeable,
-    ObjectSerdeable,
-    RemainingBytesSerdeable,
-    TransformSerdeable,
-    VariantSerdeable,
-} from "@core/serde";
-import { P, match } from "ts-pattern";
-import { ObjectSet } from "@core/object";
+import { ObjectSerdeable, RemainingBytesSerdeable, TransformSerdeable } from "@core/serde";
 import { EventBroker, SingleListenerEventBroker } from "@core/event";
 
 export const getLocalIpV4Addresses = (): string[] => {
@@ -33,51 +25,7 @@ export const getLocalIpV4Addresses = (): string[] => {
         .map((i) => schema.parse(i.address));
 };
 
-class GlobalAddressStore {
-    #reqeusted = new ObjectSet<UdpAddress>();
-    #globalAddress?: UdpAddress;
-
-    #sendControlFrame(destination: UdpAddress, frame: ControlFrame, socket: dgram.Socket): void {
-        const buffer = BufferWriter.serialize(UdpFrame.serdeable.serializer(frame)).expect("Failed to serialize frame");
-        socket.send(buffer, destination.port(), destination.humanReadableAddress());
-    }
-
-    globalAddress(): UdpAddress | undefined {
-        return this.#globalAddress;
-    }
-
-    onSendDataFrame(address: UdpAddress, socket: dgram.Socket): void {
-        if (this.#reqeusted.has(address)) {
-            return;
-        }
-
-        this.#sendControlFrame(address, new GlobalAddressRequestFrame(), socket);
-        this.#reqeusted.add(address);
-    }
-
-    onReceiveControlFrame(source: UdpAddress, frame: ControlFrame, socket: dgram.Socket): void {
-        match(frame)
-            .with(P.instanceOf(GlobalAddressRequestFrame), () => {
-                this.#sendControlFrame(source, new GlobalAddressResponseFrame({ address: source }), socket);
-            })
-            .with(P.instanceOf(GlobalAddressResponseFrame), (frame) => {
-                const address = frame.address.humanReadableAddress();
-                if (this.#globalAddress === undefined && !getLocalIpV4Addresses().includes(address)) {
-                    this.#globalAddress = frame.address;
-                }
-            })
-            .exhaustive();
-    }
-}
-
-enum FrameType {
-    Data = 1,
-    GlobalAddressRequest = 2,
-    GlobalAddressResponse = 3,
-}
-
-class DataFrame {
-    type: FrameType.Data = FrameType.Data as const;
+class UdpFrame {
     protocol: Protocol;
     payload: Uint8Array;
 
@@ -88,46 +36,13 @@ class DataFrame {
 
     static readonly serdeable = new TransformSerdeable(
         new ObjectSerdeable({ protocol: ProtocolSerdeable, payload: new RemainingBytesSerdeable() }),
-        (obj) => new DataFrame(obj),
+        (obj) => new UdpFrame(obj),
         (frame) => frame,
     );
 }
-
-class GlobalAddressRequestFrame {
-    type: FrameType.GlobalAddressRequest = FrameType.GlobalAddressRequest as const;
-
-    static readonly serdeable = new ConstantSerdeable(new GlobalAddressRequestFrame());
-}
-
-class GlobalAddressResponseFrame {
-    type: FrameType.GlobalAddressResponse = FrameType.GlobalAddressResponse as const;
-    address: UdpAddress;
-
-    constructor(args: { address: UdpAddress }) {
-        this.address = args.address;
-    }
-
-    static readonly serdeable = new TransformSerdeable(
-        new ObjectSerdeable({ address: UdpAddress.serdeable }),
-        (obj) => new GlobalAddressResponseFrame(obj),
-        (frame) => frame,
-    );
-}
-
-type ControlFrame = GlobalAddressRequestFrame | GlobalAddressResponseFrame;
-
-type UdpFrame = DataFrame | ControlFrame;
-
-const UdpFrame = {
-    serdeable: new VariantSerdeable(
-        [DataFrame.serdeable, GlobalAddressRequestFrame.serdeable, GlobalAddressResponseFrame.serdeable],
-        (frame) => frame.type,
-    ),
-} as const;
 
 export class UdpHandler implements FrameHandler {
     #selfPort: number;
-    #globalAddress = new GlobalAddressStore();
     #socket: dgram.Socket;
     #onReceive = new SingleListenerEventBroker<Frame>();
     #onClose = new EventBroker<void>();
@@ -138,25 +53,18 @@ export class UdpHandler implements FrameHandler {
         this.#socket = dgram.createSocket("udp4");
         this.#socket.on("message", (data, rinfo) => {
             const source = UdpAddress.schema.parse([rinfo.address, rinfo.port]);
-            const frame = UdpFrame.serdeable.deserializer().deserialize(new BufferReader(data));
-            if (frame.isErr()) {
-                console.warn(`Failed to deserialize frame: ${frame.unwrapErr()}`);
+            const result = UdpFrame.serdeable.deserializer().deserialize(new BufferReader(data));
+            if (result.isErr()) {
+                console.warn(`Failed to deserialize frame: ${result.unwrapErr()}`);
                 return;
             }
 
-            match(frame.unwrap())
-                .with(P.instanceOf(DataFrame), (frame) => {
-                    this.#onReceive.emit({
-                        protocol: frame.protocol,
-                        remote: new Address(source),
-                        payload: frame.payload,
-                    });
-                })
-                .with(
-                    P.union(P.instanceOf(GlobalAddressRequestFrame), P.instanceOf(GlobalAddressResponseFrame)),
-                    (frame) => this.#globalAddress.onReceiveControlFrame(source, frame, this.#socket),
-                )
-                .exhaustive();
+            const frame = result.unwrap();
+            this.#onReceive.emit({
+                protocol: frame.protocol,
+                remote: new Address(source),
+                payload: frame.payload,
+            });
         });
         this.#socket.on("listening", () => {
             const address = this.#socket.address();
@@ -168,8 +76,7 @@ export class UdpHandler implements FrameHandler {
     }
 
     address(): Address | undefined {
-        const address = this.#globalAddress.globalAddress();
-        return address && new Address(address);
+        return undefined;
     }
 
     send(frame: Frame): Result<void, LinkSendError> {
@@ -177,13 +84,12 @@ export class UdpHandler implements FrameHandler {
             throw new Error(`Expected UdpAddress, got ${frame.remote}`);
         }
 
-        const dataFrame = new DataFrame({ protocol: frame.protocol, payload: frame.payload });
+        const dataFrame = new UdpFrame({ protocol: frame.protocol, payload: frame.payload });
         this.#socket.send(
             BufferWriter.serialize(UdpFrame.serdeable.serializer(dataFrame)).expect("Failed to serialize frame"),
             frame.remote.address.port(),
             frame.remote.address.humanReadableAddress(),
         );
-        this.#globalAddress.onSendDataFrame(frame.remote.address, this.#socket);
         return Ok(undefined);
     }
 
