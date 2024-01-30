a9ff2d77c80691c228baae0e87a8766f7ee4e455
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-01-30 16:53:25 +0900

    feat(arduino): エントリとしてノードの通知を複数扱えるようにした

--- arduino/lib/net/src/net/observer/frame.h
+++ arduino/lib/net/src/net/observer/frame.h
@@ -17,6 +17,8 @@ namespace net::observer {
 
     using AsyncFrameTypeDeserializer = nb::de::Enum<FrameType, is_valid_frame_type>;
 
+    using AsyncEntryCountSerializer = nb::ser::Bin<uint8_t>;
+
     enum class NodeNotificationEntryType : uint8_t {
         SelfUpdated = 1,
         NeighborUpdated = 2,
@@ -26,24 +28,12 @@ namespace net::observer {
 
     using AsyncNodeNotificationEntryTypeSerializer = nb::ser::Enum<NodeNotificationEntryType>;
 
-    struct SelfUpdatedEntry {
-        node::OptionalClusterId cluster_id;
-        node::Cost cost;
-
-        inline static SelfUpdatedEntry from_local(const notification::SelfUpdated &self_updated) {
-            return SelfUpdatedEntry{
-                .cluster_id = self_updated.cluster_id,
-                .cost = self_updated.cost,
-            };
-        }
-    };
-
     class AsyncSelfUpdateEntrySerializer {
         node::AsyncOptionalClusterIdSerializer cluster_id_;
         node::AsyncCostSerializer cost_;
 
       public:
-        explicit AsyncSelfUpdateEntrySerializer(const SelfUpdatedEntry &self_updated)
+        explicit AsyncSelfUpdateEntrySerializer(const notification::SelfUpdated &self_updated)
             : cluster_id_{etl::move(self_updated.cluster_id)},
               cost_{etl::move(self_updated.cost)} {}
 
@@ -56,17 +46,11 @@ namespace net::observer {
         inline uint8_t serialized_length() const {
             return cluster_id_.serialized_length() + cost_.serialized_length();
         }
-    };
 
-    struct NeighborUpdatedEntry {
-        node::NodeId neighbor;
-        node::Cost link_cost;
-
-        inline static NeighborUpdatedEntry
-        from_local(const notification::NeighborUpdated &neighbor_updated) {
-            return NeighborUpdatedEntry{
-                .neighbor = neighbor_updated.neighbor_id, .link_cost = neighbor_updated.link_cost
-            };
+        static inline uint8_t serialized_length(const notification::SelfUpdated &self_updated) {
+            return node::AsyncOptionalClusterIdSerializer::serialized_length(self_updated.cluster_id
+                   ) +
+                node::AsyncCostSerializer::serialized_length(self_updated.cost);
         }
     };
 
@@ -75,8 +59,10 @@ namespace net::observer {
         node::AsyncCostSerializer link_cost_;
 
       public:
-        explicit AsyncNeighborUpdatedEntrySerializer(const NeighborUpdatedEntry &neighbor_updated)
-            : neighbor_{etl::move(neighbor_updated.neighbor)},
+        explicit AsyncNeighborUpdatedEntrySerializer(
+            const notification::NeighborUpdated &neighbor_updated
+        )
+            : neighbor_{etl::move(neighbor_updated.neighbor_id)},
               link_cost_{etl::move(neighbor_updated.link_cost)} {}
 
         template <nb::AsyncWritable W>
@@ -89,14 +75,11 @@ namespace net::observer {
         inline uint8_t serialized_length() const {
             return neighbor_.serialized_length() + link_cost_.serialized_length();
         }
-    };
 
-    struct NeighborRemovedEntry {
-        node::NodeId neighbor_id;
-
-        inline static NeighborRemovedEntry
-        from_local(const notification::NeighborRemoved &neighbor_removed) {
-            return NeighborRemovedEntry{neighbor_removed.neighbor_id};
+        static inline uint8_t
+        serialized_length(const notification::NeighborUpdated &neighbor_updated) {
+            return node::AsyncNodeIdSerializer::serialized_length(neighbor_updated.neighbor_id) +
+                node::AsyncCostSerializer::serialized_length(neighbor_updated.link_cost);
         }
     };
 
@@ -104,7 +87,9 @@ namespace net::observer {
         node::AsyncNodeIdSerializer neighbor_id_;
 
       public:
-        explicit AsyncNeighborRemovedEntrySerializer(const NeighborRemovedEntry &neighbor_removed)
+        explicit AsyncNeighborRemovedEntrySerializer(
+            const notification::NeighborRemoved &neighbor_removed
+        )
             : neighbor_id_{etl::move(neighbor_removed.neighbor_id)} {}
 
         template <nb::AsyncWritable W>
@@ -115,13 +100,18 @@ namespace net::observer {
         inline uint8_t serialized_length() const {
             return neighbor_id_.serialized_length();
         }
+
+        static inline uint8_t
+        serialized_length(const notification::NeighborRemoved &neighbor_removed) {
+            return node::AsyncNodeIdSerializer::serialized_length(neighbor_removed.neighbor_id);
+        }
     };
 
     struct FrameReceivedEntry {};
 
     class AsyncFrameReceivedEntrySerializer {
       public:
-        explicit AsyncFrameReceivedEntrySerializer(const FrameReceivedEntry &) {}
+        explicit AsyncFrameReceivedEntrySerializer(const notification::FrameReceived &) {}
 
         template <nb::AsyncWritable W>
         inline nb::Poll<nb::SerializeResult> serialize(W &buffer) {
@@ -131,34 +121,17 @@ namespace net::observer {
         inline uint8_t serialized_length() const {
             return 0;
         }
-    };
 
-    using NodeNotificationEntry = etl::
-        variant<SelfUpdatedEntry, NeighborUpdatedEntry, NeighborRemovedEntry, FrameReceivedEntry>;
-
-    class AsyncNodeNotificationEntrySerializer {
-        nb::ser::Bin<uint8_t> frame_type_{static_cast<uint8_t>(FrameType::NodeNotification)};
-        nb::ser::Variant<
-            AsyncSelfUpdateEntrySerializer,
-            AsyncNeighborUpdatedEntrySerializer,
-            AsyncNeighborRemovedEntrySerializer,
-            AsyncFrameReceivedEntrySerializer>
-            notification_;
-
-      public:
-        explicit AsyncNodeNotificationEntrySerializer(const NodeNotificationEntry &notification)
-            : notification_{notification} {}
-
-        template <nb::AsyncWritable W>
-        inline nb::Poll<nb::SerializeResult> serialize(W &buffer) {
-            SERDE_SERIALIZE_OR_RETURN(frame_type_.serialize(buffer));
-            return notification_.serialize(buffer);
-        }
-
-        inline uint8_t serialized_length() const {
-            return frame_type_.serialized_length() + notification_.serialized_length();
+        static inline uint8_t serialized_length(const notification::FrameReceived &) {
+            return 0;
         }
     };
+
+    using AsyncNodeNotificationEntrySerializer = nb::ser::Variant<
+        AsyncSelfUpdateEntrySerializer,
+        AsyncNeighborUpdatedEntrySerializer,
+        AsyncNeighborRemovedEntrySerializer,
+        AsyncFrameReceivedEntrySerializer>;
 
     struct NodeSubscriptionFrame {};
 
@@ -175,22 +148,38 @@ namespace net::observer {
         }
     };
 
-    inline NodeNotificationEntry
-    from_local_notification(const notification::LocalNotification &notification) {
-        return etl::visit<NodeNotificationEntry>(
-            util::Visitor{
-                [](const notification::SelfUpdated &self_updated) {
-                    return SelfUpdatedEntry::from_local(self_updated);
-                },
-                [](const notification::NeighborUpdated &neighbor_updated) {
-                    return NeighborUpdatedEntry::from_local(neighbor_updated);
-                },
-                [](const notification::NeighborRemoved &neighbor_removed) {
-                    return NeighborRemovedEntry::from_local(neighbor_removed);
-                },
-                [](const notification::FrameReceived &) { return FrameReceivedEntry{}; }
-            },
-            notification
+    struct NodoNotificationFrameMetadata {
+        uint8_t entry_count;
+        uint8_t serialized_length;
+    };
+
+    inline NodoNotificationFrameMetadata get_frame_metadata(notification::NotificationService &nts
+    ) {
+        uint8_t entry_count = nts.size();
+        if (entry_count == 0) {
+            return {.entry_count = 0, .serialized_length = 0};
+        }
+
+        uint8_t entry_length = etl::accumulate(
+            nts.begin(), nts.end(), 0,
+            [](uint8_t acc, const notification::LocalNotification &notification) {
+                return acc + AsyncNodeNotificationEntrySerializer::serialized_length(notification);
+            }
         );
+
+        uint8_t length = entry_length + AsyncEntryCountSerializer::serialized_length(entry_count);
+        return {.entry_count = entry_count, .serialized_length = length};
+    }
+
+    inline void serialize_frame(
+        frame::FrameBufferWriter &writer,
+        NodoNotificationFrameMetadata metadata,
+        notification::NotificationService &nts
+    ) {
+        writer.serialize_all_at_once(AsyncEntryCountSerializer{metadata.entry_count});
+        for (const auto &notification : nts) {
+            writer.serialize_all_at_once(AsyncNodeNotificationEntrySerializer{notification});
+        }
+        FASSERT(writer.is_all_written());
     }
 } // namespace net::observer
