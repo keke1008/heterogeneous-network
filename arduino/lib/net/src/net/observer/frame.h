fa215587ed074063fb9f1ed8dab2f4bb6d650812
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-01-31 13:27:17 +0900

    feat(arduino): 時の：度を完全に同期するNodeSyncのフレームを実装

--- arduino/lib/net/src/net/observer/frame.h
+++ arduino/lib/net/src/net/observer/frame.h
@@ -3,4 +3,5 @@
 // IWYU pragma: begin_exports
 #include "./frame/notification.h"
 #include "./frame/subscription.h"
+#include "./frame/sync.h"
 // IWYU pragma: end_exports
---
+++ arduino/lib/net/src/net/observer/frame/sync.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "../constants.h"
+#include "./frame_type.h"
+#include <net/frame.h>
+#include <net/routing.h>
+
+namespace net::observer {
+    class NodeSyncFrameWriter {
+        using NeighborCountSerializer = nb::ser::Bin<uint8_t>;
+
+        uint8_t
+        calculate_length(const local::LocalNodeInfo &info, const neighbor::NeighborService &ns) {
+            uint8_t length = 0;
+
+            length += AsyncFrameTypeSerializer::serialized_length(FrameType::NodeSync);
+
+            length += node::AsyncSourceSerializer::serialized_length(info.source);
+            length += node::AsyncCostSerializer::serialized_length(info.cost);
+
+            length += NeighborCountSerializer::serialized_length(ns.get_neighbor_count());
+            ns.for_each_neighbor_node([&](const neighbor::NeighborNode &neighbor) {
+                length += node::AsyncNodeIdSerializer::serialized_length(neighbor.id());
+                length += node::AsyncCostSerializer::serialized_length(neighbor.link_cost());
+            });
+
+            return length;
+        }
+
+        frame::FrameBufferReader write_frame(
+            const local::LocalNodeInfo &info,
+            const neighbor::NeighborService &ns,
+            frame::FrameBufferWriter &&writer
+        ) {
+            writer.serialize_all_at_once(node::AsyncSourceSerializer(info.source));
+            writer.serialize_all_at_once(node::AsyncCostSerializer(info.cost));
+
+            writer.serialize_all_at_once(NeighborCountSerializer(ns.get_neighbor_count()));
+            ns.for_each_neighbor_node([&](const neighbor::NeighborNode &neighbor) {
+                writer.serialize_all_at_once(node::AsyncNodeIdSerializer(neighbor.id()));
+                writer.serialize_all_at_once(node::AsyncCostSerializer(neighbor.link_cost()));
+            });
+
+            FASSERT(writer.is_all_written());
+            return writer.create_reader();
+        }
+
+      public:
+        nb::Poll<frame::FrameBufferReader> execute(
+            frame::FrameService &fs,
+            const local::LocalNodeService &lns,
+            neighbor::NeighborService &ns,
+            routing::RoutingSocket<FRAME_DELAY_POOL_SIZE> &socket,
+            const node::Destination &destination,
+            util::Rand &rand
+
+        ) {
+            const auto &info = POLL_UNWRAP_OR_RETURN(lns.poll_info());
+            uint8_t length = calculate_length(info, ns);
+
+            auto &&writer = POLL_MOVE_UNWRAP_OR_RETURN(
+                socket.poll_frame_writer(fs, lns, rand, destination, length)
+            );
+            return write_frame(info, ns, etl::move(writer));
+        }
+    };
+} // namespace net::observer
