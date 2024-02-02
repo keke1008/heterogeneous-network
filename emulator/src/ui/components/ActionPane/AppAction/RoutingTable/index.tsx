83d4de33ca3d35adc5b294173d9880d639517f7c
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-02-02 16:39:13 +0900

    refactor(emulator): 不必要なログを削除

--- emulator/src/ui/components/ActionPane/AppAction/RoutingTable/index.tsx
+++ emulator/src/ui/components/ActionPane/AppAction/RoutingTable/index.tsx
@@ -14,7 +14,6 @@ const InnerApp: React.FC<InnerAppProps> = ({ client }) => {
     client.onUpdateEntries(setEntries);
 
     const handleUpdate = (entry: RoutingEntry) => {
-        console.debug("update", entry);
         client.updateEntries([entry]);
     };
     const handleDelete = (entry: RoutingEntry) => {
