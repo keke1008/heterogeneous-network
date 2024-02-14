34716d42b3823a51924f81f98948fff07420817e
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-02-14 09:27:17 +0900

    refactor(arduino): 使用していないテスト削除

--- arduino/test/net/test_frame/service.cpp
+++
@@ -1,8 +0,0 @@
-#include <doctest.h>
-
-#include <net/frame.h>
-
-TEST_CASE("instantiation") {
-    memory::Static<net::frame::MultiSizeFrameBufferPool<1, 2>> pool;
-    net::frame::FrameService service{pool};
-}
